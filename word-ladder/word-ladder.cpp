29cf344197024265723c1b3d0554527a0944c759
icode09 <74093530+icode09@users.noreply.github.com>
2021-12-30 11:36:15 +0530

    Time: 179 ms (55.25%), Space: 13.9 MB (76.72%) - LeetHub

--- word-ladder/word-ladder.cpp
+++ word-ladder/word-ladder.cpp
@@ -1,36 +1,34 @@
 class Solution {
 public:
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
-        unordered_set<string> dict(wordList.begin(),wordList.end());
-        if(dict.find(endWord) == dict.end()) {
-            return 0;
-        }
-        queue<string> q;
-        q.push(beginWord);
-        int steps = 1;
-        while(!q.empty()){
-            int size = q.size();
-            while(size--){
-                string word = q.front();q.pop();
-                
-                if(word == endWord) return steps;
-                
-                dict.erase(word);
-                for(int i = 0;i<word.size();i++){
-                    char ch = word[i];
-                    for(int j = 0 ; j< 26;j++){
-                        word[i] = 'a' + j;
-                        if(dict.find(word) != dict.end()){
-                            q.push(word);
-                            dict.erase(word);
-                        }
-                    }
-                    word[i] = ch;
-                }
+        unordered_set<string> wordDict(wordList.begin(), wordList.end());
+        queue<string> toVisit;
+        addNextWords(beginWord, wordDict, toVisit);
+        int dist = 2;
+        while (!toVisit.empty()) {
+            int num = toVisit.size();
+            for (int i = 0; i < num; i++) {
+                string word = toVisit.front();
+                toVisit.pop();
+                if (word == endWord) return dist;
+                addNextWords(word, wordDict, toVisit);
             }
-            steps++;
+            dist++;
         }
-        
         return 0;
     }
+private:
+    void addNextWords(string word, unordered_set<string>& wordDict, queue<string>& toVisit) {
+        for (int p = 0; p < (int)word.length(); p++) {
+            char letter = word[p];
+            for (int k = 0; k < 26; k++) { 
+                word[p] = 'a' + k;
+                if (wordDict.find(word) != wordDict.end()) {
+                    toVisit.push(word);
+                    wordDict.erase(word);
+                }
+            }
+            word[p] = letter;
+        } 
+    } 
 };
