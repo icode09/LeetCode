6c433f290313151aa05947ad6f362f2a4a6de914
icode09 <74093530+icode09@users.noreply.github.com>
2021-12-31 15:37:04 +0530

    Time: 3 ms (54.42%), Space: 5.9 MB (72.57%) - LeetHub

---
+++ sqrtx/sqrtx.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        if(x == 1) return 1;
+        int low = 1, high = x/2;
+        while(low <= high){
+            int mid = low + (high-low)/2;
+            if(mid == x/mid) return mid;
+            else if(mid > x/mid) high = mid-1;
+            else low = mid+1;
+        }
+        
+        return high;
+    }
+};
