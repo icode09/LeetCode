d417ae194410e67b3350cb2051b32492fbe130d2
icode09 <74093530+icode09@users.noreply.github.com>
2022-01-15 11:29:45 +0530

    Time: 94 ms (48.47%), Space: 39.3 MB (98.85%) - LeetHub

--- 654-maximum-binary-tree/654-maximum-binary-tree.cpp
+++ 654-maximum-binary-tree/654-maximum-binary-tree.cpp
@@ -11,44 +11,19 @@
  */
 class Solution {
 public:
-    unordered_map<int,TreeNode*> mapka;
-    int findMaxIndex(vector<int>& nums,int start,int end){
-        
-        int index = start;
-        int maxVal = nums[start];
-        for(int i = start;i<=end;i++){
-            if(nums[i] > maxVal){
-                maxVal = nums[i];
-                index = i;
-            }
-        }
-        
-        return index;
-    }
-    TreeNode* maximumTree(vector<int>& nums,int start,int end){
-        if(start > end ) return NULL;
-        
-        int index = findMaxIndex(nums,start,end); // O(n) operation
-        
-        if(mapka.find(nums[index]) != mapka.end()) return mapka[nums[index]];
-        
-        TreeNode* root =  new TreeNode(nums[index]);
-        mapka[nums[index]] = root;
-        
-        
-        root->left = maximumTree(nums,start,index - 1);
-        root->right = maximumTree(nums,index + 1,end);
-        
-        return root;
-        
-    }
     TreeNode* constructMaximumBinaryTree(vector<int>& nums) {
-        int n = nums.size();
-        if(n == 0) return NULL;
-        if(n == 1) return new TreeNode(nums[0]);
-        
-        mapka.clear();
-        
-        return maximumTree(nums,0,n-1);
+        vector<TreeNode*> stack;
+        for(auto num : nums){
+            TreeNode* curr = new TreeNode(num);
+            while(!stack.empty() && stack.back()->val < curr->val){
+                curr->left = stack.back();
+                stack.pop_back();
+            }
+            if(!stack.empty()){
+                stack.back()->right = curr;
+            }
+            stack.push_back(curr);
+        }
+        return stack.front();
     }
 };
