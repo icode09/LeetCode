c3adeaa846f3572c7b06dae074f9efc65250ff17
icode09 <74093530+icode09@users.noreply.github.com>
2022-01-16 12:17:32 +0530

    Time: 31 ms (39.18%), Space: 24 MB (53.29%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/238-product-of-array-except-self.cpp
@@ -2,20 +2,15 @@ class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& arr) {
         int n = arr.size();
-        vector<int> prefix(n);
-        prefix[0] = 1;
-        
-        for(int i = 1;i<n;i++){
-            prefix[i] = prefix[i-1]*arr[i-1];
-        }
-        
-        vector<int> suffix(n);
         vector<int> res(n);
-        suffix[n-1] = 1;
-        res[n-1] = suffix[n-1]*prefix[n-1];
-        for(int i = n-2;i>=0;i--){
-            suffix[i] = suffix[i+1]*arr[i+1];
-            res[i] = prefix[i] * suffix[i];
+        res[0] = 1;
+        for(int i = 1;i<n;i++){
+            res[i] = res[i-1]*arr[i-1];
+        }
+        int right = 1;
+        for(int i = n-1;i>=0;i--){
+            res[i] *= right;
+            right *= arr[i];
         }
         
         return res;
