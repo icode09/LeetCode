9855b16efb71d72e6a67b4ef6e0ffcc9f7e0df1b
icode09 <74093530+icode09@users.noreply.github.com>
2022-01-19 18:57:09 +0530

    Time: 238 ms (5.03%), Space: 56.9 MB (81.88%) - LeetHub

--- 701-insert-into-a-binary-search-tree/701-insert-into-a-binary-search-tree.cpp
+++ 701-insert-into-a-binary-search-tree/701-insert-into-a-binary-search-tree.cpp
@@ -14,12 +14,23 @@ public:
     TreeNode* insertIntoBST(TreeNode* root, int val) {
         if(!root) return new TreeNode(val);
         
-        if(root->val < val){
-            root->right = insertIntoBST(root->right,val);
-        }else{
-            root->left = insertIntoBST(root->left,val);
+        TreeNode* curr = root;
+        while(curr){
+            if(curr->val < val) {
+                if(curr->right) curr = curr->right;
+                else {
+                    curr->right = new TreeNode(val);
+                    break;
+                }
+            }
+            else {
+                if(curr->left) curr = curr->left;
+                else{
+                    curr->left = new TreeNode(val);
+                    break;
+                }
+            }
         }
-        
         return root;
     }
 };
