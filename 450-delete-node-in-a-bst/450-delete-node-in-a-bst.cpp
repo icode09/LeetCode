f1f3be86aa634c9a62b2f8bdd1e1079f6547f8ad
icode09 <74093530+icode09@users.noreply.github.com>
2022-01-21 10:39:05 +0530

    Time: 44 ms (50.12%), Space: 32.7 MB (32.75%) - LeetHub

--- 450-delete-node-in-a-bst/450-delete-node-in-a-bst.cpp
+++ 450-delete-node-in-a-bst/450-delete-node-in-a-bst.cpp
@@ -11,32 +11,46 @@
  */
 class Solution {
 public:
-    TreeNode* findInorderSucc(TreeNode* root, int key){
+    TreeNode* findInorderSucc(TreeNode* root){
         TreeNode* curr = root->right;
         while(curr->left){
             curr = curr->left;
         }
         return curr;
     }
+    TreeNode* deleteThisNode(TreeNode* root){
+        if(!root) return NULL;
+        
+        if(!root->left) return root->right;
+        else if(!root->right) return root->left;
+        else{
+            TreeNode* nextNode = findInorderSucc(root);
+            nextNode->left = root->left;
+            return root->right;
+        }
+    }
     TreeNode* deleteNode(TreeNode* root, int key) {
         if(!root) return NULL;
         
-        if(root->val < key){
-            root->right = deleteNode(root->right,key);
-        }else if(root->val > key){
-            root->left = deleteNode(root->left,key);
-        }else{
-            // 3 Conditions => 1. No child, 2. Single child, 3. both childs
-            if(!root->left && !root->right) return NULL;
-            else if(!root->left || !root->right){
-                return (root->left) ? root->left : root->right;
-            }
-            else{
-                TreeNode* nextNode = findInorderSucc(root,key);
-                root->val = nextNode->val;
-                root->right = deleteNode(root->right,nextNode->val);
+        TreeNode* curr = root, *prev = NULL;
+        while(curr && curr->val != key){
+            prev = curr;
+            if(curr->val < key){
+                curr = curr->right;
+            }else{
+                curr = curr->left;
             }
         }
+        
+        if(!prev) return deleteThisNode(root);
+        
+        if(prev->left == curr){
+            prev->left = deleteThisNode(curr);
+        }
+        else {
+            prev->right = deleteThisNode(curr);
+        }
+        
         return root;
     }
 };
