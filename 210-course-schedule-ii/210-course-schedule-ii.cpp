cb8b9f804b052cec9f7bcbbec4f5b61056d16fb0
icode09 <74093530+icode09@users.noreply.github.com>
2022-01-21 13:07:36 +0530

    Time: 20 ms (75.20%), Space: 14.5 MB (33.54%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.cpp
+++ 210-course-schedule-ii/210-course-schedule-ii.cpp
@@ -1,35 +1,58 @@
 class Solution {
 public:
-    vector<int> findOrder(int n, vector<vector<int>>& arr) {
-        vector<int> adj[n];
-        vector<int> indegree(n,0);
-        for(auto it : arr){
-            adj[it[1]].push_back(it[0]);
-            indegree[it[0]]++;
-        }
+    void dfs(vector<int> adj[],vector<int> &vis,int node,stack<int> &st){
+        vis[node]=1;
         
-        queue<int> q;
-        for(int i = 0;i<n;i++){
-            if(indegree[i] == 0){
-                q.push(i);
+        for(auto x: adj[node]){
+            if(!vis[x]){
+                dfs(adj,vis,x,st);
             }
         }
-       
-        vector<int> topOrder;
         
-        while(!q.empty()){
-            int u = q.front();
-            q.pop();
-            n--;
-            topOrder.push_back(u);
+        st.push(node);
+    }
+     bool iscycle(vector<int> adj[],vector<int> &vis,int id){
+        if(vis[id]==1)
+            return true;
+        if(vis[id]==0){
+            vis[id]=1;
+            for(auto edge : adj[id]){
+                if(iscycle(adj,vis,edge))
+                    return true;
+            }
+        }
+        vis[id] = 2;
+        return false;
+    }
+    vector<int> findOrder(int n, vector<vector<int>>& pre) {
+         vector<int> adj[n];
+        for(auto edge : pre)
+            adj[edge[1]].push_back(edge[0]);
+        vector<int> vis(n,0);
+        
+        vector<int> ans;
+        for(int i=0;i<n;i++){
+            if(iscycle(adj,vis,i))
+                return ans;
+        }
+        
+      
+        stack<int> st;
+        fill(begin(vis) , end(vis) , 0);
+        
+        for(int i=0;i<n;i++){
+            if(!vis[i]){
+                dfs(adj,vis,i,st);
+            }
+        }
+        
+        
+        while(!st.empty()){
             
-            for(auto v : adj[u]){
-                if(--indegree[v] == 0)
-                    q.push(v);
-            }
+            ans.push_back(st.top());
+            st.pop();
         }
         
-        if(n == 0) return topOrder;
-        else return {};
+        return ans;
     }
 };
