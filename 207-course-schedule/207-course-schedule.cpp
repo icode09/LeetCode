570ba3fb6b6cca97ae1b9597bb68877b8b6a1884
icode09 <74093530+icode09@users.noreply.github.com>
2022-01-21 12:02:12 +0530

    Time: 43 ms (21.79%), Space: 13.8 MB (61.00%) - LeetHub

--- 207-course-schedule/207-course-schedule.cpp
+++ 207-course-schedule/207-course-schedule.cpp
@@ -1,39 +1,29 @@
 class Solution {
 public:
-    bool dfs(vector<int> adj[],vector<bool>& visited,vector<bool>& recStack,int u){
-        visited[u] = true;
-        recStack[u] = true;
-        
-        for(auto v : adj[u]){
-            if(!visited[v]){
-                if(dfs(adj,visited,recStack,v)) return true;
-            }
-            else if(recStack[v]) return true;
-        }
-        
-        recStack[u] = false;
-        
-        return false;
-    }
-    bool isCyclic(vector<int> adj[],int V){
-        vector<bool> visited(V,false);
-        vector<bool> recStack(V,false);
-        
-        for(int i = 0;i<V;i++){
-            if(!visited[i])
-                if(dfs(adj,visited,recStack,i)) return true;
-        }
-        
-        return false;
-    }
     bool canFinish(int V, vector<vector<int>>& arr) {
-        // Check cycle in a directed graph
         vector<int> adj[V];
+        vector<int> indegree(V,0);
         for(auto it : arr){
             adj[it[1]].push_back(it[0]);
+            indegree[it[0]]++;
+        }
+        queue<int> q;
+        for(int i = 0;i<V;i++){
+            if(indegree[i] == 0)
+                q.push(i);
         }
         
+        while(!q.empty()){
+            int u = q.front();
+            q.pop();
+            V--;
+            
+            for(auto v : adj[u]){
+                if(--indegree[v] == 0)
+                    q.push(v);
+            }
+        }
         
-       return isCyclic(adj,V) ? false : true;
+        return (V == 0);
     }
 };
