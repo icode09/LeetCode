b10f6c8b2573149cda6ad239733f72c856d1305f
icode09 <74093530+icode09@users.noreply.github.com>
2022-01-23 23:39:13 +0530

    Time: 219 ms (26.16%), Space: 63.4 MB (57.17%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
@@ -4,17 +4,18 @@ public:
         heights.push_back(0);
         int size = heights.size();
         
-        int i = 0, maxArea = 0;
+        int maxArea = 0;
         stack<int> st;
-        while(i < size){
-            if(st.empty() || heights[st.top()] <= heights[i]){
-                st.push(i++);
-            }else{
-                int top = st.top();
+        
+        for(int i = 0;i<size;i++){
+            while(!st.empty() && heights[st.top()] > heights[i]){
+                int height = heights[st.top()];
                 st.pop();
                 
-                maxArea = max(maxArea , heights[top]*(st.empty() ? i : i - st.top() - 1));
+                int l = (st.empty()) ? -1  : st.top();
+                maxArea = max(maxArea, height*(i - l -1));
             }
+            st.push(i);
         }
         
         return maxArea;
