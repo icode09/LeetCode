85715a1e9ba9fba059871a15b43ebfc4d2559612
icode09 <74093530+icode09@users.noreply.github.com>
2022-02-14 23:06:27 +0530

    Time: 11 ms (63.80%), Space: 9.6 MB (97.86%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -4,12 +4,14 @@ public:
         int n = grid.size();
         int m = grid[0].size();
         
-        for(int j = 1;j<m;j++){
-            grid[0][j] += grid[0][j-1];
-        }
+        for(int i = 1;i<m;i++)
+            grid[0][i] += grid[0][i-1];
         
         for(int i = 1;i<n;i++){
             grid[i][0] += grid[i-1][0];
+        }
+        
+        for(int i = 1;i<n;i++){
             for(int j = 1;j<m;j++){
                 grid[i][j] += min(grid[i-1][j] , grid[i][j-1]);
             }
