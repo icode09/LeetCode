40c017d4c6e6265c5e2d7a0d67528172b874bf2b
icode09 <74093530+icode09@users.noreply.github.com>
2022-02-16 05:11:02 +0530

    Added solution - LeetHub

---
+++ Run Length Encoding - GFG/run-length-encoding.cpp
@@ -0,0 +1,42 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+string encode(string src);    
+ 
+int main() {
+	
+	int T;
+	cin>>T;
+	while(T--)
+	{
+		string str;
+		cin>>str;
+		
+		cout<<encode(str)<<endl;
+	}
+	return 0;
+}// } Driver Code Ends
+
+
+/*You are required to complete this function */
+
+string encode(string src)
+{     
+  //Your code here 
+  string res = "";
+  int n = src.length();
+  
+  for(int i = 0;i<n;i++){
+      int count = 1;
+      while(i < n-1 && src[i] == src[i+1]){
+          count++;
+          i++;
+      }
+      res.push_back(src[i]);
+      res.append(to_string(count));
+  }
+  
+  return res;
+}     
+ 
