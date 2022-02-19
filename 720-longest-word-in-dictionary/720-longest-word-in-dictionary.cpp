765784518c40a2a358a88ff3828b15df1165cc7a
icode09 <74093530+icode09@users.noreply.github.com>
2022-02-19 17:44:52 +0530

    Time: 77 ms (55.00%), Space: 35.8 MB (18.75%) - LeetHub

--- 720-longest-word-in-dictionary/720-longest-word-in-dictionary.cpp
+++ 720-longest-word-in-dictionary/720-longest-word-in-dictionary.cpp
@@ -1,18 +1,56 @@
 class Solution {
 public:
-    string longestWord(vector<string>& words) {
-        sort(words.begin(),words.end());
+    struct TrieNode{
+      vector<TrieNode*> children;
+        string word;
+        bool isEnd;
+        
+        TrieNode(){
+            this->word = "";
+            this->isEnd = false;
+            this->children = vector<TrieNode*>(26,NULL);
+        }
+    };
+    
+    
+    void insert(TrieNode* root,string& word){
+        TrieNode* curr = root;
+        for(int i = 0;i<(int)word.size();i++){
+            int idx = word[i] - 'a';
+            if(!curr->children[idx])
+                curr->children[idx] = new TrieNode();
+            
+            curr = curr->children[idx];
+        }
+        curr->isEnd = true;
+        curr->word = word;
+    }
+    string findLongestWord(TrieNode* root){
         string res = "";
-        unordered_set<string> st;
-        for(string& word : words){
-            if(word.length() == 1 || st.find(word.substr(0,word.length() - 1)) != st.end()){
-                if(word.length() > res.length()){
-                    res = word;
+        queue<TrieNode*> q;
+        q.push(root);
+        while(!q.empty()){
+            int size = q.size();
+            while(size--){
+                TrieNode* front = q.front();
+                q.pop();
+                for(int i = 25;i>=0;i--){
+                    if(front->children[i] && front->children[i]->isEnd){
+                        res = front->children[i]->word;
+                        q.push(front->children[i]);
+                    }
                 }
-                st.insert(word);
             }
         }
-        
         return res;
+    }
+    string longestWord(vector<string>& words) {
+        TrieNode* root = new TrieNode();
+         
+        for(auto w : words){
+            insert(root,w);
+        }
+        
+        return findLongestWord(root);
     }
 };
