b2534cd9b4de821191905552920375562a92ef1b
icode09 <74093530+icode09@users.noreply.github.com>
2022-02-20 19:02:59 +0530

    Time: 124 ms (93.36%), Space: 75.3 MB (74.41%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -3,22 +3,18 @@ public:
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
         
-        int leftMin = prices[0] , rightMax = prices[n-1];
-        int left[n] , right[n];
-        left[0] = 0;  right[n-1] = 0;
-        for(int i = 1 , j = n-2 ; i < n && j >= 0 ; i++ , j--){
-            leftMin = min(leftMin , prices[i]);
-            left[i] = max(left[i-1] , prices[i] - leftMin);
-            rightMax = max(rightMax , prices[j]);
-            right[j] = max(right[j+1] , rightMax - prices[j]);
-        }
-        
-        int maxxProfit = 0;
+        int buy1 = INT_MAX;
+        int profit1 = INT_MIN;
+        int buy2 = INT_MAX;
+        int profit2 = INT_MIN;
         
         for(int i = 0;i<n;i++){
-            maxxProfit = max(maxxProfit, left[i] + right[i]);
+            buy1 = min(buy1 , prices[i]);
+            profit1 = max(profit1 , prices[i] - buy1);
+            buy2 = min(buy2 , prices[i] - profit1);
+            profit2 = max(profit2 , prices[i] - buy2);
         }
         
-        return maxxProfit;
+        return profit2;
     }
 };
