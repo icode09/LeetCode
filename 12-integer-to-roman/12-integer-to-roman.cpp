344ebbcf25ec82633620ab807220fb9f2feaa560
icode09 <74093530+icode09@users.noreply.github.com>
2022-02-26 13:10:11 +0530

    Time: 25 ms (16.95%), Space: 6 MB (52.61%) - LeetHub

--- 12-integer-to-roman/12-integer-to-roman.cpp
+++ 12-integer-to-roman/12-integer-to-roman.cpp
@@ -1,17 +1,12 @@
 class Solution {
 public:
-    string intToRoman(int num) {
-        string res;
-        string sym[] = { "M" , "CM", "D" , "CD" , "C" ,"XC","L" ,"XL","X","IX","V", "IV", "I"};
-        int val[] = {1000 , 900, 500 , 400, 100, 90, 50,40,10,9,5,4,1};
-        
-        for(int i = 0;num != 0 ;i++){
-            while(num >= val[i]){
-                num -= val[i];
-                res += sym[i];
-            }
-        }
-        
-        return res;
+    string intToRoman(int n) {
+       string ones[] = {"" , "I" , "II" , "III" , "IV" , "V" , "VI" , "VII" , "VIII" , "IX"};
+	string tens[] = {"" , "X" , "XX" , "XXX" , "XL", "L" , "LX", "LXX", "LXXX", "XC"};
+	string hundreds[] = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+	string thousands[] = {"" , "M", "MM", "MMM"};
+
+
+	return thousands[(n / 1000)] + hundreds[(n % 1000) / 100] + tens[(n % 100) / 10] + ones[n % 10];
     }
 };
