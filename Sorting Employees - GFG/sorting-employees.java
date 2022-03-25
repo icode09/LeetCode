8e04fa549a46b08ebd752c6ca8d62a74b1299a97
icode09 <74093530+icode09@users.noreply.github.com>
2022-03-25 19:02:43 +0530

    Added solution - LeetHub

---
+++ Sorting Employees - GFG/sorting-employees.java
@@ -0,0 +1,94 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+
+
+//Initial Template for Java
+
+
+
+import java.io.*;
+import java.util.*;
+
+class node
+{
+    int salary;
+    String name;
+    public void setSalary(int a)
+    {
+        this.salary=a;
+    }
+    public void setName(String s1)
+    {
+        this.name=s1;
+    }
+}
+
+class Array {
+
+    // Driver code
+    public static void main(String[] args) throws IOException {
+        // Taking input using buffered reader
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int testcases = Integer.parseInt(br.readLine());
+        // looping through all testcases
+        while (testcases-- > 0) {
+            int n = Integer.parseInt(br.readLine());
+            String line = br.readLine();
+            String[] q = line.trim().split("\\s+");
+            node a[]=new node[n];
+            for(int i=0;i<2*n;i=i+2) {
+                a[i / 2] = new node();
+                a[i / 2].setName(q[i]);
+                a[i / 2].setSalary(Integer.parseInt(q[i + 1]));
+            }
+
+            Solution ob = new Solution();
+            ob.sortRecords(a,n);
+            for(int i=0;i<n;i++)
+            {
+                System.out.print(a[i].name+" "+a[i].salary+" ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+/*
+class node
+{
+    int salary;
+    String name;
+    public void setSalary(int a)
+    {
+        this.salary=a;
+    }
+    public void setName(String s)
+    {
+        this.name=s;
+    }
+}
+*/
+
+
+class Solution
+{
+    void sortRecords(node arr[],int n)
+    {
+        // Your code goes here
+        Arrays.sort(arr, (e1,e2)->{
+            if(e1.salary == e2.salary){
+                return e1.name.compareTo(e2.name);
+            }
+            return e1.salary - e2.salary;
+        });
+    }
+}
