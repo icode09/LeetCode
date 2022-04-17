4037a9b3603d14a6d53921efb03939590076e482
icode09 <74093530+icode09@users.noreply.github.com>
2022-04-17 19:22:42 +0530

    Added solution - LeetHub

--- Index Of an Extra Element - GFG/index-of-an-extra-element.cpp
+++ Index Of an Extra Element - GFG/index-of-an-extra-element.cpp
@@ -10,12 +10,14 @@ class Solution{
         int low = 0 ,high = n-1;
         while(low < high){
             int mid = low + (high - low)/2;
-            if(a[mid] >= b[mid]) low = mid + 1;
-            else high = mid;
+            if(a[mid] < b[mid]) high = mid;
+            else low = mid + 1;
         }
         
         return low;
     }
+    // 2 3 5 6 9 10 12
+    // 2 5 6 9 10 12
 };
 
 // { Driver Code Starts.
