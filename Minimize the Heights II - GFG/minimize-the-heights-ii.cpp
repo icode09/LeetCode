93bb8194fa7979583a45428e4f506725c88e6403
icode09 <74093530+icode09@users.noreply.github.com>
2022-04-23 16:09:37 +0530

    Added solution - LeetHub

---
+++ Minimize the Heights II - GFG/minimize-the-heights-ii.cpp
@@ -0,0 +1,48 @@
+// { Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+// User function template for C++
+
+class Solution {
+  public:
+    int getMinDiff(int arr[], int n, int k) {
+        // code here
+        sort(arr, arr + n);
+        int minDiff = arr[n-1] - arr[0];
+        int smallest = arr[0] + k;
+        int largest = arr[n-1] - k;
+        for(int i = 1;i<=n-1;i++){
+            if(arr[i] - k < 0)
+                continue;
+                
+            int minn = min(arr[i] - k , smallest);
+            int maxx = max(arr[i-1] + k , largest);
+            minDiff = min(minDiff , maxx - minn);
+        }
+        
+        return minDiff;
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, k;
+        cin >> k;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.getMinDiff(arr, n, k);
+        cout << ans << "\n";
+    }
+    return 0;
+}  // } Driver Code Ends
